d795d7e60c7440fdf37534d5b40dabc1c25a179f
nik27090 <nik27090@mail.ru>
2020-02-10 15:09:52 +0300

    connect db

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,21 +1,23 @@
 package main;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
+import dbService.DBException;
+import dbService.DBService;
+import dbService.dataSets.UsersDataSet;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+    public static void main(String[] args) {
+        DBService dbService = new DBService();
+        dbService.printConnectInfo();
+        try {
+            long userId =dbService.addUser("tully");
+            System.out.println("Added user id: " + userId);
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+            UsersDataSet dataSet = dbService.getUser(userId);
+            System.out.println("User data set: " + dataSet);
 
-        Server server = new Server(8080);
-        server.setHandler(context);
-
-        server.start();
-        server.join();
+            dbService.cleanUp();
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
     }
 }
