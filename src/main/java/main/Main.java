059cad4617904a6966b6a5ff7f4ec4537a79aea4
nik27090 <nik27090@mail.ru>
2020-02-16 15:40:10 +0300

    exmpl webSocket

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,27 @@
 package main;
 
+import chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
-
         Server server = new Server(8080);
-        server.setHandler(context);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase("templates");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resourceHandler, context});
+        server.setHandler(handlers);
 
         server.start();
         server.join();
